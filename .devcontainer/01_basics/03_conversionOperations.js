a34c07f8c3af73a6fc0ae1fc9ebbe725e1a982a2
Giggle2000 <139992936+Giggle2000@users.noreply.github.com>
2025-07-10 16:33:25 +0000

    conversions of datatype

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = true
+console.log(typeof(score));
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);// results in NaN(not a number)
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false =>0
+let isLoggedIn ="hitesh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//""=>false
+// "mk"=>false
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+ 
