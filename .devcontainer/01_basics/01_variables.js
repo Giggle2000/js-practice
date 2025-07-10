847f34d35b7e37db2202326b1c0a01633ffab23a
Giggle2000 <139992936+Giggle2000@users.noreply.github.com>
2025-07-10 10:14:50 +0000

    var,const,let done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 144553
+let accountEmail = 'Mk@gmail.com'
+var accountPassword = '14435'
+let accountCity = 'chandigarh'
+console.table([accountId, accountPassword, accountEmail, accountCity]);
